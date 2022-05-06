07c4d94ce42eba3cfe7fed2f38d830e2d5007d95
samiulislam09 <samiulislamsawon09@gmail.com>
2022-05-06 20:54:01 +0600

    connected to mongo db

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-const port = process.env.port || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+const port = process.env.port || 5000;
+
+const app = express();
 app.use(cors())
+app.use(express.json())
 
-
-const uri = "mongodb+srv://book-warehouse:3hUs0OxIAkoMSHxj@cluster0.mrq9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://bookwarehouse:X1b9ZYpVMQWH3Zhc@cluster0.ewdqd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res)=>{
     res.send("hello guys");
 });
+
+const run = async ()=>{
+    try{
+        await client.connect()
+        console.log('connected')
+    }catch(error){
+        console.log(error)
+
+    }
+}
+run()
+
 app.listen(port, ()=>{
     console.log("hello guys im running in", port)
 })
